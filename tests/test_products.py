e114a82ca0c65b891e3484e1f077da8eeebfb551
Yuri <rspnsk@mail.ru>
2025-11-24 02:16:49 +0300

    добавленны новые функции и тесты

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,4 +1,4 @@
-from src.categories_products import Product
+from src.categories import Product
 
 
 def test_init() -> None:
@@ -8,10 +8,10 @@ def test_init() -> None:
     )
 
     # Проверяем, что все атрибуты установлены правильно
-    assert product.name == "Смартфон", "Ошибка в имени продукта"
-    assert product.description == "Современный смартфон с большим экраном", "Ошибка в описании"
-    assert product.price == 29999.99, "Ошибка в цене"
-    assert product.quantity == 10, "Ошибка в количестве"
+    assert product.name == "Смартфон"
+    assert product.description == "Современный смартфон с большим экраном"
+    assert product.price == 29999.99
+    assert product.quantity == 10
 
 
 def test_init_types() -> None:
@@ -27,10 +27,27 @@ def test_init_types() -> None:
     assert isinstance(product.quantity, int), "Количество должно быть целым числом"
 
 
-def test_quantity() -> None:
-    """Тест проверки количества товара"""
-    # Проверяем начальное количество
-    product = Product(
-        name="Смартфон", description="Современный смартфон с большим экраном", price=29999.99, quantity=10
-    )
-    assert product.quantity == 10, "Ошибка в начальном количестве"
+def test_new_product(product_dict):
+    """ тест проверки создания нового экземпляра из словаря"""
+    assert Product.new_product(product_dict).name == "Iphone 15"
+    assert Product.new_product(product_dict).description == "512GB, Gray space"
+    assert Product.new_product(product_dict).price == 210000.0
+    assert Product.new_product(product_dict).quantity == 8
+
+
+# Тестирование геттера и сеттера
+def test_price_getter_and_setter(apple_product):
+    # Проверка первоначального значения цены
+    assert apple_product.price == 210000.0
+
+    # Установим новое значение цены
+    apple_product.price = 220000.0
+
+    # Проверим, что цена успешно обновилась
+    assert apple_product.price == 220000.0
+
+    # Попробуем установить некорректную цену (например, отрицательное значение)
+    apple_product.price = -10.0
+
+    # Проверим, что цена осталась старой после неудачной попытки изменения
+    assert apple_product.price == 220000.0
